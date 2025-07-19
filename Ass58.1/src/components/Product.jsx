21f860c7aac15b8b1f8af23ffdc0d73783277167
Govindvarshney001 <Govind.work001@example.com>
2025-07-19 14:11:37 +0530

    Added more code/features

---
+++ Ass58.1/src/components/Product.jsx
@@ -0,0 +1,37 @@
+// components/Product.jsx
+import React from "react";
+import { Link } from "react-router-dom";
+import ProductDetails from "./ProductDetails";
+
+const Product = ({ title, price, tag, image, sale, oldPrice,id }) => {
+  return (
+    <div
+      // onClick={onClick}
+      className="cursor-pointer border rounded-lg p-4 hover:shadow-lg transition-all"
+    >
+      <img
+        src={image}
+        alt={title}
+        className="w-full h-48 object-cover rounded-md"
+      />
+      <div className="mt-4 space-y-1">
+        <h2 className="font-semibold">{title}</h2>
+        <p className="text-sm text-gray-500">{tag}</p>
+        <div className="flex items-center gap-2">
+          <p className="font-bold text-black">{price}</p>
+          {sale === "true" && oldPrice && (
+            <p className="line-through text-red-500 text-sm">{oldPrice}</p>
+          )}
+        </div>
+        <Link
+          to={`/product/1`}
+          className="text-blue-500 underline mt-2 inline-block"
+        >
+          View Product
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Product;
